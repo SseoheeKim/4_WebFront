01c1e2e2a2f76e10614dd9d12ef2ccd8db5c32cb
Sseohee <108666056+SseoheeKim@users.noreply.github.com>
2022-11-07 11:35:35 +0900

    js

---
+++ 3_Javascript/js/09_배열.js
@@ -0,0 +1,12 @@
+document.getElementById("btn1").addEventListener("click", function(){
+
+    const arr1 = new Array();
+    const arr2 = new Array(3);
+    const arr3 = [];
+    const arr4 = ['사과', '딸기', '바나나'];
+
+    console.log(arr1);
+    console.log(arr2);
+    console.log(arr3);
+    console.log(arr4);
+});
