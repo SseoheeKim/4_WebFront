cbdc63c5bea8720fd1b979d5c6ff3586ee49eb4d
Sseohee <108666056+SseoheeKim@users.noreply.github.com>
2022-11-07 11:55:18 +0900

    js

--- 3_Javascript/js/09_배열.js
+++ 3_Javascript/js/09_배열.js
@@ -9,4 +9,28 @@ document.getElementById("btn1").addEventListener("click", function(){
     console.log(arr2);
     console.log(arr3);
     console.log(arr4);
+
+
+    // 배열에 존재하지 않는 인덱스에 값 대입
+    // -> 자동으로 인덱스가 추가되면서 길이가 증가되고, 자료형은 자동으로 동일하게 삽입
+
+    arr1[0] = "김밥";
+    arr1[1] = 5000
+    arr1[2] = true;
+
+    arr1[5]
+
+    // for문으로 배열 요소 반복 접근하기
+    // 1. 일반for문
+    for(let i=0; i<arr4.length; i++){
+        console.log(arr4[i]);
+    }
+    
+    // 2. 배열.forEach( function(item,index){반복 수행 코드} )  -> 배열에만 사용
+    // - item : 현재 접근중인 요소
+    // - index : 현재 인덱스
+
+    arr4.forEach(function(arr,i) {
+        console.lon(i +":" + arr);
+    });
 });
